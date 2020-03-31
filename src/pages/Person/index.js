637dba9780a7dfdce624c28d34e0b060cc402d6e
Gleydson Miranda Souza <gleydson.m.s.1993@gmail.com>
2020-03-31 11:04:48 -0300

    inicio do desenvolvimento da interface person

--- src/pages/Person/index.js
+++ src/pages/Person/index.js
@@ -1,18 +1,61 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { View, Image, Text,TouchableOpacity, FlatList, TextInput } from 'react-native';
 
 import logoImg from '../../assets/logo.png';
+import titleImg from '../../assets/dados.png';
 import styles from './styles';
+//import { TextInput } from 'react-native-gesture-handler';
 
 export default function Person () {
+    const navigation = useNavigation();
+
+    function navigateToRunTest() {
+        navigation.navigate('RunTest');
+    }
     return(
         <View style={styles.container}>
             <View style={styles.header}>
                 <Image style={styles.image} source={logoImg} />
-                <Text style={styles.headerTitle}>
-                    DIGITE OS DADOS
-                </Text>
+                <Image style={styles.imageTitle} source={titleImg} />
             </View>
+            <FlatList
+                data={[1]}
+                keyExtractor={message => String(message)}
+                showsVerticalScrollIndicator={false}
+                renderItem={() => (
+                    <View>
+                        <View style={styles.viewForm}>
+                            <Text style={styles.text}>Nome</Text>
+                            <TextInput style={styles.textInput}></TextInput>
+                        </View>
+                        <View style={styles.viewFormN}>
+                            <Text style={styles.text}>Sexo </Text>
+                            
+                        </View>
+                        <View style={styles.viewFormN}>
+                            <Text style={styles.text}>Idade </Text>
+                            <TextInput style={styles.textInput}></TextInput>
+                        </View>
+                        <View style={styles.viewFormN}>
+                            <Text style={styles.text}>Peso  </Text>
+                            <TextInput style={styles.textInput}></TextInput>
+                        </View>
+                        <View style={styles.viewFormN}>
+                            <Text style={styles.text}>Estatura</Text>
+                            <TextInput style={styles.textInput}></TextInput>
+                        </View>
+                        <View style={styles.viewButton}>
+                            <TouchableOpacity
+                                style={styles.button}
+                                onPress={navigateToRunTest}
+                            >
+                                <Text style={styles.buttonText}>PRÓXIMO</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                )}
+            />
         </View>
     );
 }
--- src/pages/Person/styles.js
+++ src/pages/Person/styles.js
@@ -19,12 +19,72 @@ export default StyleSheet.create({
     image: {
         width: 140,
         height: 90,
+        borderRadius: 16,
     },
 
-    headerTitle: {
-        color: '#fff',
+    imageTitle: {
+        width: 165,
+        height: 60,
+    },
+
+    viewForm: {
+        flex:1,
+        marginTop: 30,
+        paddingTop: 30,
+        paddingBottom: 0,
+        paddingHorizontal: 5,
+        justifyContent: "space-between",
+        alignItems: "center",
+        flexDirection: 'row',
+    },
+
+    viewFormN: {
+        flex:1,
+        marginTop: 10,
+        paddingTop: 10,
+        paddingBottom: 0,
+        paddingHorizontal: 5,
+        justifyContent: "space-between",
+        alignItems: "center",
+        flexDirection: 'row',
+    },
+
+    text: {
+        color: "#fff",
+        fontWeight: "bold",
+        fontSize: 30,
+        fontFamily: "Roboto",
+        paddingRight: 15,
+    },
+
+    textInput: {
+        flex:1,
+        width: 240,
+        height: 40,
+        borderRadius: 8,
+        fontSize: 20,
+        backgroundColor: '#fff',
+    },
+
+    viewButton: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 40,
+        marginBottom:40,
+    },
+
+    button: {
+        backgroundColor: '#fff',
+        width: 160,
+        height: 60,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 16,
+    },
+
+    buttonText: {
         fontWeight: 'bold',
-        fontSize: 24,
+        fontSize: 16,
         fontFamily: 'Roboto',
     },
 });
