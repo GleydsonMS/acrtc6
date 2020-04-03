06725bbe342ca7fc891f25d81a6a8988a7a08eb1
Gleydson Miranda Souza <gleydson.m.s.1993@gmail.com>
2020-04-02 22:51:28 -0300

    início da implementação da interface RunTest

--- src/pages/RunTest/index.js
+++ src/pages/RunTest/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import { View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { View, Image } from 'react-native';
+
+import logoImg from '../../assets/logo.png';
+import titleImg from '../../assets/teste.png';
+import styles from './styles';
 
 export default function RunTest () {
     return(
-        <View/>
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image style={styles.image} source={logoImg} />
+                <Image style={styles.imageTitle} source={titleImg} />
+            </View>
+        </View>
     );
 }
---
+++ src/pages/RunTest/styles.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: 24,
+        paddingTop: Constants.statusBarHeight + 20,
+        backgroundColor: "#00B386",
+        opacity: 0.6,
+    },
+
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+
+    image: {
+        width: 140,
+        height: 90,
+        borderRadius: 16,
+    },
+
+    imageTitle: {
+        width: 180,
+        height: 70,
+    },
+});
