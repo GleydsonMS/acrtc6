f2b916d575954aa0643c0dd5f82745962f97264a
Gleydson Miranda Souza <gleydson.m.s.1993@gmail.com>
2020-04-03 20:50:47 -0300

    desenvolvimento da interface Runway

--- src/pages/Runway/index.js
+++ src/pages/Runway/index.js
@@ -1,8 +1,81 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { View, Image, Text, TouchableOpacity, FlatList, TextInput } from 'react-native';
 
-export default function Runway () {
+import logoImg from '../../assets/logo.png';
+import titleImg from '../../assets/teste.png';
+import styles from './styles';
+
+export default function Person () {
+    const navigation = useNavigation();
+    const [pista, setPista] = useState('');
+    const [voltas, setVoltas] = useState('');
+    const [sobra, setSobra] = useState('');
+
+    function handleResults () {
+        const dataPista = { pista, voltas, sobra };
+
+        navigation.navigate('Results', dataPista);
+    }
+
     return(
-        <View/>
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image style={styles.image} source={logoImg} />
+                <Image style={styles.imageTitle} source={titleImg} />
+            </View>
+            <FlatList
+                data={[1]}
+                keyExtractor={message => String(message)}
+                showsVerticalScrollIndicator={false}
+                renderItem={() => (
+                    <View>
+                        <View style={styles.viewForm}>
+                            <Text style={styles.text}>COMPRIMENTO DA PISTA</Text>
+                           
+                            <TextInput 
+                                style={styles.textInput}
+                                placeholder="EM METROS"
+                                placeholderTextColor='#666'
+                                autoCapitalize='none'
+                                autoCorrect={false}
+                                value={pista}
+                                onChangeText={setPista}
+                            />
+                        </View>
+                        <View style={styles.viewFormN}>
+                            <Text style={styles.text}>NÚMERO DE VOLTAS</Text>
+                            <TextInput 
+                                style={styles.textInput}
+                                autoCapitalize='none'
+                                autoCorrect={false}
+                                value={voltas}
+                                onChangeText={setVoltas}
+                            />
+                        </View>
+                        <View style={styles.viewFormN}>
+                            <Text style={styles.text}>METRAGEM ADICIONAL</Text>
+                            <TextInput 
+                                style={styles.textInput}
+                                placeholder="EM METROS"
+                                placeholderTextColor='#666'
+                                autoCapitalize='none'
+                                autoCorrect={false}
+                                value={sobra}
+                                onChangeText={setSobra}
+                            />
+                        </View>
+                        <View style={styles.viewButton}>
+                            <TouchableOpacity
+                                style={styles.button}
+                                onPress={handleResults}
+                            >
+                                <Text style={styles.buttonText}>CALCULAR</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                )}
+            />
+        </View>
     );
 }
---
+++ src/pages/Runway/styles.js
@@ -0,0 +1,95 @@
+import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: 24,
+        paddingTop: Constants.statusBarHeight + 20,
+        backgroundColor: "#00B386",
+        opacity: 0.6,
+    },
+
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+
+    image: {
+        width: 140,
+        height: 90,
+        borderRadius: 16,
+    },
+
+    imageTitle: {
+        width: 165,
+        height: 60,
+    },
+
+    viewForm: {
+        flex:1,
+        marginTop: 40,
+        paddingTop: 40,
+        paddingBottom: 0,
+        paddingHorizontal: 5,
+        justifyContent: "space-between",
+        alignItems: "center",
+        flexDirection: 'row',
+    },
+
+    viewFormN: {
+        flex:1,
+        marginTop: 30,
+        paddingTop: 30,
+        paddingBottom: 0,
+        paddingHorizontal: 5,
+        justifyContent: "space-between",
+        alignItems: "center",
+        flexDirection: 'row',
+    },
+
+    text: {
+        flex:1,
+        flexDirection: "row",
+        color: "#fff",
+        fontWeight: "bold",
+        fontSize: 20,
+        fontFamily: "Roboto",
+        paddingRight: 15,
+        textAlign: "center",
+    },
+
+    textInput: {
+        flex:1,
+        width: 240,
+        height: 40,
+        borderRadius: 8,
+        fontSize: 20,
+        paddingHorizontal: 10,
+        backgroundColor: '#fff',
+    },
+
+    viewButton: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 50,
+        paddingTop: 50,
+        marginBottom:40,
+    },
+
+    button: {
+        backgroundColor: '#fff',
+        width: 160,
+        height: 60,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 16,
+    },
+
+    buttonText: {
+        fontWeight: 'bold',
+        fontSize: 16,
+        fontFamily: 'Roboto',
+    },
+});
