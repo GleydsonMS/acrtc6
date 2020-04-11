786073c694c655033b1604486fe452f43e3b98bd
Gleydson Miranda Souza <gleydson.m.s.1993@gmail.com>
2020-04-11 08:48:41 -0300

    mudança de flatlist para scrowview na interface Person

--- src/pages/Person/index.js
+++ src/pages/Person/index.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { View, Image, Text,TouchableOpacity, FlatList, TextInput } from 'react-native';
+import { View, Image, Text, TouchableOpacity, ScrollView, TextInput } from 'react-native';
 import { RadioButton } from 'react-native-paper';
 
 import logoImg from '../../assets/logo.png';
 import titleImg from '../../assets/dados.png';
 import styles from './styles';
 
-export default function Person () {
+export default function Person() {
     const navigation = useNavigation();
     const [nome, setNome] = useState('');
     const [sexo, setSexo] = useState('');
@@ -17,94 +17,89 @@ export default function Person () {
 
     const data = { nome, sexo, idade, peso, estatura };
 
-    function handlePerson () {
+    function handlePerson() {
         navigation.navigate('RunTest', { data });
     }
 
-    return(
+    return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <Image style={styles.image} source={logoImg} />
                 <Image style={styles.imageTitle} source={titleImg} />
             </View>
-            <FlatList
-                data={[1]}
-                keyExtractor={message => String(message)}
-                showsVerticalScrollIndicator={false}
-                renderItem={() => (
-                    <View>
-                        <View style={styles.viewForm}>
-                            <Text style={styles.text}>Nome</Text>
-                            <TextInput 
-                                style={styles.textInput}
-                                autoCapitalize='none'
-                                autoCorrect={false}
-                                value={nome}
-                                onChangeText={setNome}
-                            />
-                        </View>
-                        <View style={styles.viewFormN}>
-                            <Text style={styles.text}>Sexo </Text>
-                            <Text style={styles.text}>M</Text>
-                            <RadioButton 
-                                color="#fff"
-                                value="1"
-                                status={sexo == '1' ? 'checked' : 'unchecked'}
-                                onPress={() => setSexo('1')}
-                            />
-                            <Text style={styles.text}>F</Text>
-                            <RadioButton 
-                                 color="#fff"
-                                value= '0'
-                                status={sexo == '0' ? 'checked' : 'unchecked'}
-                                onPress={() => setSexo('0')}
-                            />
-                        </View>
-                        <View style={styles.viewFormN}>
-                            <Text style={styles.text}>Idade </Text>
-                            <TextInput 
-                                style={styles.textInput}
-                                autoCapitalize='none'
-                                autoCorrect={false}
-                                value={idade}
-                                onChangeText={setIdade}
-                            />
-                        </View>
-                        <View style={styles.viewFormN}>
-                            <Text style={styles.text}>Peso  </Text>
-                            <TextInput 
-                                style={styles.textInput}
-                                placeholder="EM KG"
-                                placeholderTextColor='#666'
-                                autoCapitalize='none'
-                                autoCorrect={false}
-                                value={peso}
-                                onChangeText={setPeso}
-                            />
-                        </View>
-                        <View style={styles.viewFormN}>
-                            <Text style={styles.text}>Estatura</Text>
-                            <TextInput 
-                                style={styles.textInput}
-                                placeholder="EM METROS"
-                                placeholderTextColor='#666'
-                                autoCapitalize='none'
-                                autoCorrect={false}
-                                value={estatura}
-                                onChangeText={setEstatura}
-                            />
-                        </View>
-                        <View style={styles.viewButton}>
-                            <TouchableOpacity
-                                style={styles.button}
-                                onPress={() => handlePerson()}
-                            >
-                                <Text style={styles.buttonText}>PRÓXIMO</Text>
-                            </TouchableOpacity>
-                        </View>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View>
+                    <View style={styles.viewForm}>
+                        <Text style={styles.text}>Nome</Text>
+                        <TextInput
+                            style={styles.textInput}
+                            autoCapitalize='none'
+                            autoCorrect={false}
+                            value={nome}
+                            onChangeText={setNome}
+                        />
                     </View>
-                )}
-            />
+                    <View style={styles.viewFormN}>
+                        <Text style={styles.text}>Sexo </Text>
+                        <Text style={styles.text}>M</Text>
+                        <RadioButton
+                            color="#fff"
+                            value="1"
+                            status={sexo == '1' ? 'checked' : 'unchecked'}
+                            onPress={() => setSexo('1')}
+                        />
+                        <Text style={styles.text}>F</Text>
+                        <RadioButton
+                            color="#fff"
+                            value='0'
+                            status={sexo == '0' ? 'checked' : 'unchecked'}
+                            onPress={() => setSexo('0')}
+                        />
+                    </View>
+                    <View style={styles.viewFormN}>
+                        <Text style={styles.text}>Idade </Text>
+                        <TextInput
+                            style={styles.textInput}
+                            autoCapitalize='none'
+                            autoCorrect={false}
+                            value={idade}
+                            onChangeText={setIdade}
+                        />
+                    </View>
+                    <View style={styles.viewFormN}>
+                        <Text style={styles.text}>Peso  </Text>
+                        <TextInput
+                            style={styles.textInput}
+                            placeholder="EM KG"
+                            placeholderTextColor='#666'
+                            autoCapitalize='none'
+                            autoCorrect={false}
+                            value={peso}
+                            onChangeText={setPeso}
+                        />
+                    </View>
+                    <View style={styles.viewFormN}>
+                        <Text style={styles.text}>Estatura</Text>
+                        <TextInput
+                            style={styles.textInput}
+                            placeholder="EM METROS"
+                            placeholderTextColor='#666'
+                            autoCapitalize='none'
+                            autoCorrect={false}
+                            value={estatura}
+                            onChangeText={setEstatura}
+                        />
+                    </View>
+                    <View style={styles.viewButton}>
+                        <TouchableOpacity
+                            style={styles.button}
+                            onPress={() => handlePerson()}
+                        >
+                            <Text style={styles.buttonText}>PRÓXIMO</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </ScrollView>
         </View>
     );
 }
