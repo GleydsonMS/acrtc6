a2fd6ce0ef7a169e8a7ce7d740478d66fdf7a895
Gleydson Miranda Souza <gleydson.m.s.1993@gmail.com>
2020-04-11 08:25:34 -0300

    mudança de flatlist para scrowview na interface Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { View, Image, Text, TouchableOpacity, FlatList, Linking } from 'react-native';
+import { View, Image, Text, TouchableOpacity, ScrollView, Linking } from 'react-native';
 
 import logoImg from '../../assets/logo.png';
 import titleImg from '../../assets/orientacoes.png';
@@ -19,41 +19,36 @@ export default function Main() {
                 <Image style={styles.image} source={logoImg} />
                 <Image style={styles.imageTitle} source={titleImg} />
             </View>
-            <FlatList
-                data={[1]}
-                keyExtractor={message => String(message)}
-                showsVerticalScrollIndicator={false}
-                renderItem={() => (
-                    <View>
-                        <View style={styles.viewMessage}>
-                            <Text style={styles.message}>
-                                Esta é uma ferramenta para lhe auxiliar na avaliação dos níveis
-                                de aptidão cardiorrespiratória de pessoas entre 10 e 14 anos. 
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View>
+                    <View style={styles.viewMessage}>
+                        <Text style={styles.message}>
+                            Esta é uma ferramenta para lhe auxiliar na avaliação dos níveis
+                            de aptidão cardiorrespiratória de pessoas entre 10 e 14 anos.
                             </Text>
-                            <Text style={styles.message2}>
-                                O objetivo é calcular o VO2máximo de forma indireta utilizando
-                                o teste de corrida de 6 minutos (TC6).
+                        <Text style={styles.message2}>
+                            O objetivo é calcular o VO2máximo de forma indireta utilizando
+                            o teste de corrida de 6 minutos (TC6).
                             </Text>
-                            <Text style={styles.message2}> 
-                                O teste segue as recomendações do PROESP Brasil. Para mais
-                                informações sobre este projeto acesse a página pelo link
+                        <Text style={styles.message2}>
+                            O teste segue as recomendações do PROESP Brasil. Para mais
+                            informações sobre este projeto acesse a página pelo link
                             </Text>
-                            <Text style={styles.linkSite}
-                                onPress={() => Linking.openURL('https://www.ufrgs.br/proesp/')}>
-                                https://www.ufrgs.br/proesp/
+                        <Text style={styles.linkSite}
+                            onPress={() => Linking.openURL('https://www.ufrgs.br/proesp/')}>
+                            https://www.ufrgs.br/proesp/
                             </Text>
-                        </View>
-                        <View style={styles.viewButton}>
-                            <TouchableOpacity
-                                style={styles.button}
-                                onPress={navigateToInstructionsFirst}
-                            >
-                                <Text style={styles.buttonText}>IR PARA TESTE</Text>
-                            </TouchableOpacity>
-                        </View>
                     </View>
-                )}
-            />
+                    <View style={styles.viewButton}>
+                        <TouchableOpacity
+                            style={styles.button}
+                            onPress={navigateToInstructionsFirst}
+                        >
+                            <Text style={styles.buttonText}>IR PARA TESTE</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </ScrollView>
         </View>
     );
 }
